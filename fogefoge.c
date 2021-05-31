f781c4c1d764fe778d95961cd46fd6ddf2253cf2
Nirlan Souza <nirlan858@gmail.com>
2021-05-31 04:14:49 -0300

    Imprime mapa alocado dinamicamente na memoria e movimenta personagem

---
+++ fogefoge.c
@@ -0,0 +1,97 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "fogefoge.h"
+
+char** mapa;
+int linhas;
+int colunas;
+
+void liberamapa() {
+    for(int i = 0; i < linhas; i++) {
+        free(mapa[i]);        
+    }
+    free(mapa);
+}
+
+void alocamapa() {
+    mapa = malloc(sizeof(char*) * linhas);
+    for(int i = 0; i < linhas; i++) {
+        mapa[i] = malloc(sizeof(char) * (colunas+1));
+    }
+}
+
+void lemapa() {
+    FILE* f;
+    f = fopen("mapa.txt", "r");
+    if(f == 0){
+        printf("Erro na leitura do mapa\n");
+        exit(1);
+    }
+
+    fscanf(f, "%d %d", &linhas, &colunas);   
+
+    alocamapa();
+
+    for(int i = 0; i < linhas; i++) {
+        fscanf(f, "%s", mapa[i]);
+    }
+    fclose(f);
+}
+
+void imprimemapa() {
+    for(int i = 0; i < linhas; i++) {
+        printf("%s\n", mapa[i]);
+    }
+}
+
+int acabou() {
+    return 0;
+}
+
+void move(char direcao) {
+    int x, y;
+
+    for(int i = 0; i < linhas; i++) {
+        for(int j = 0; j < colunas; j++) {
+            if(mapa[i][j] == '@'){
+                x = i;
+                y = j;
+            }
+        }
+    }
+
+    switch(direcao) {
+        case 'a':
+            mapa[x][y-1] = '@';
+            break;
+        case 'w':
+            mapa[x-1][y] = '@';
+            break;
+        case 's':
+            mapa[x+1][y] = '@';
+            break;
+        case 'd':
+            mapa[x][y+1] = '@';
+            break;
+    }
+
+    mapa[x][y] = '.';
+    
+}
+
+int main() {
+    
+    lemapa();
+
+    do {
+        imprimemapa();
+
+        char comando;
+        scanf(" %c", &comando);
+        move(comando);
+
+    } while(!acabou());
+    
+    liberamapa();   
+
+}
---
+++ fogefoge.h
@@ -0,0 +1,6 @@
+void liberamapa();
+void lemapa();
+void alocamapa();
+void move(char direcao);
+void imprimemapa();
+int acabou();
